d9df432846380c133d982ea4a92b3126c6359502
bote795 <nbotell2@gmail.com>
2015-03-11 14:44:10 -0500

    Created a GameScreen class

---
+++ core/src/com/mygdx/game/ZBGame.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
+public class ZBGame extends Game {
+    @Override
+    public void create() {
+        Gdx.app.log("ZBGame", "Created");
+    }
+}
+
