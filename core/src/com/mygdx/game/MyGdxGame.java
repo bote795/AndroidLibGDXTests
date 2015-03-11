e6be0f2497ed24f5c8f7c08920a7f1d7fbe8384c
bote795 <nbotell2@gmail.com>
2015-03-11 11:35:40 -0500

    added animation moving with atlas

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,39 +2,41 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private BitmapFont font;
+    private SpriteBatch batch;
+    private TextureAtlas textureAtlas;
+    private Animation animation;
+    private float elapsedTime = 0;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.GREEN);
-        font.scale(5);
+        textureAtlas = new TextureAtlas(Gdx.files.internal("data/spritesheet.atlas"));
+        animation = new Animation(1/15f, textureAtlas.getRegions());
 	}
 
     @Override
     public void dispose() {
         batch.dispose();
-        font.dispose();
+        textureAtlas.dispose();
     }
 
     @Override
 	public void render () {
         //0,0 is bottom left
         //white canvas
-        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         //finish clearing
 
         batch.begin();
         //drawing is done here
-        font.draw(batch,"First libgdx APP",100,400);
+        elapsedTime += Gdx.graphics.getDeltaTime();
+        batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
         batch.end();
 	}
 }
