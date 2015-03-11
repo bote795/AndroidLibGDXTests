08a962879ec642899fb0f33dad3b06a528b2ccfe
bote795 <nbotell2@gmail.com>
2015-03-10 21:47:07 -0500

    clean up

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,26 +3,20 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
-    private Texture img;
-    private Sprite sprite;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
-        img = new Texture("badlogic.jpg");
-        sprite= new Sprite(img);
 	}
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
     }
 
     @Override
@@ -35,7 +29,6 @@ public class MyGdxGame extends ApplicationAdapter {
 
         batch.begin();
         //drawing is done here
-        sprite.draw(batch);
         batch.end();
 	}
 }
