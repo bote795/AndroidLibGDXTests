d8650e7fbf34d200b16a2188e37b8045289b5aa1
bote795 <nbotell2@gmail.com>
2015-03-11 15:26:37 -0500

    added bird class and created it in game world then passed in the coordinates for where its going to start

--- core/src/com/mydx/screens/GameScreen/GameScreen.java
+++ core/src/com/mydx/screens/GameScreen/GameScreen.java
@@ -13,8 +13,13 @@ public class GameScreen implements Screen{
     private GameRenderer renderer;
 
     public GameScreen(){
-        Gdx.app.log("GameScreen", "Attached");
-        world = new GameWorld();
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float gameWidth = 136;
+        float gameHeight = screenHeight / (screenWidth / gameWidth);
+
+        int midPointY = (int) (gameHeight / 2);
+        world = new GameWorld(midPointY);
         renderer = new GameRenderer(world);
     }
     @Override
--- core/src/com/mygdx/GameWorld/GameWorld.java
+++ core/src/com/mygdx/GameWorld/GameWorld.java
@@ -1,23 +1,21 @@
 package com.mygdx.GameWorld;
 
-import com.badlogic.gdx.Gdx;
-
-import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.gameobjects.Bird;
 
 /**
  * Created by HULK on 3/11/2015.
  */
 public class GameWorld {
-    private Rectangle rect = new Rectangle(0,0,17,12);
+    private Bird bird;
+    public GameWorld(int midPointY) {
+        bird = new Bird(33, midPointY - 5, 17, 12);
+    }
 
     public void update(float delta){
-        Gdx.app.log("GameWorld", "Update");
-        rect.x++;
-        if(rect.x >137){
-            rect.x=0;
-        }
+        bird.update(delta);
     }
-    public Rectangle getRect(){
-        return rect;
+
+    public Bird getBird() {
+        return bird;
     }
 }
