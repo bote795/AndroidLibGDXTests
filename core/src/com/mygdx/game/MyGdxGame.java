0f0dccfa0f7c362c457a0af818d309c7e1c5f2ae
bote795 <nbotell2@gmail.com>
2015-03-10 21:39:02 -0500

    How to add Text to screen

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,40 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private BitmapFont font;
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.GREEN);
+        font.scale(5);
+	}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+    @Override
+	public void render () {
+        //0,0 is bottom left
+        //white canvas
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //finish clearing
+
+        batch.begin();
+        //drawing is done here
+        font.draw(batch,"First libgdx APP",100,400);
+        batch.end();
+	}
+}
